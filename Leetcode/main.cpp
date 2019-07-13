91ae4d1fad490435f458a922f6239bc56a58ca4b
yangxia <yangxiamike@gmail.com>
2019-07-13 10:19:46 +0800

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by xiayang on 2019/7/13.
+//  Copyright © 2019 xiayang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
